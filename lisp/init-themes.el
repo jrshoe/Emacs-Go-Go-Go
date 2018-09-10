630f2061c75d3a0bfffa22cf9fab3a393ff57fcd
Shawn Xu <xuuexu@gmail.com>
2018-09-10 15:58:00 +0800

    Add some favorite themes and fonts

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,10 +1,17 @@
 (paradox-require 'tao-theme)
 (paradox-require 'doom-themes)
 
+
+
+;; my favorite themes
 (load-theme 'doom-nova t)
 ;; (load-theme 'tao-yin t)
-;; set font
+;; (load-theme 'github)
+;; (load-theme 'eclipse)
+
+;; my favorite fonts
 (set-frame-font "Source Code Pro 14" nil t)
+;; (set-frame-font "Hermit 14" nil t)
 
 (paradox-require 'default-text-scale)
 (add-hook 'after-init-hook 'default-text-scale-mode)
