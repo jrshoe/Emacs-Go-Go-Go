8adcc663100c3f571c652acf392931b76ef5e6e8
Shawn Xu <xuuexu@gmail.com>
2018-09-26 13:04:43 +0800

    company setup

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -12,15 +12,11 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; copy from somewhere
-;; for better experience
-(if (fboundp 'evil-declare-change-repeat)
-    (mapc #'evil-declare-change-repeat
-	  '(company-complete-common
-	    company-select-next
-	    company-select-previous
-	    company-complete-selection
-	    company-complete-number)))
+
+;; more fluent when typing
+(setq company-require-match nil)
+
+;; delay setup
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 3)
 
